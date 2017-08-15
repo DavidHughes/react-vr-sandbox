ee3c930e9f2c30cd3c7adca41c28eb8714fb5493
David Hughes <david.hughes125@gmail.com>
2017-08-15 20:02:27 +0100

    Add some 3d objects & lighting.

--- index.vr.js
+++ index.vr.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {
   AppRegistry,
+  AmbientLight,
   asset,
+  DirectionalLight,
   Pano,
   Text,
   View,
+  Cylinder,
 } from 'react-vr';
 
 export default class react_vr_sandbox extends React.Component {
@@ -24,8 +27,43 @@ export default class react_vr_sandbox extends React.Component {
             textAlignVertical: 'center',
             transform: [{translate: [0, 0, -3]}],
           }}>
-          hello
+          Hello
         </Text>
+        <Text
+          style={{
+            position: 'absolute',
+            transform: [{translate: [0, 0, -2]}],
+            layoutOrigin: [0.5, 0.5],
+            backgroundColor: '#2E008B'
+          }}
+          fontSize={0.4}>
+          Office
+        </Text>
+        <Cylinder
+          radiusTop={0.3}
+          radiusBottom={0.8}
+          dimHeight={2}
+          segments={36}
+          style={{
+            color: '#fff',
+            backgroundColor: '#000',
+            transform: [{translate: [1, 1, -1]}],
+          }}
+          lit
+        />
+        <AmbientLight
+          intensity={0.3}
+          style={{
+            color: 'red'
+          }}
+        />
+        <DirectionalLight
+          intensity={2}
+          style={{
+            color: 'blue',
+            transform: [{translate: [1, 0, -1.5]}]
+          }}
+        />
       </View>
     );
   }
